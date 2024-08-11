194f45b8cba0d0bb9e2151ad1a2218ca1ffe67b4
wallace_brown <brownwdev@gmail.com>
2024-08-11 16:17:01 -0700

    Iterate on the init script.
    * Detect if a docker container already exists
    * Run sqlx migrations

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 ### Installing Sqlx
 * cargo install --version="~0.7" sqlx-cli --no-default-features --features rustls,postgres
+
+### Creating Migrations
+Run the following
+
+* export DATABASE_URL=postgres://postgres:password@localhost:5432/newsletter
+* sqlx migrate add create_subscriptions_table
+
+You'll see a new file created in /migrations. Add your sql there.
+
+### Running Migrations
+Run the following
+
+* export DATABASE_URL=postgres://postgres:password@localhost:5432/newsletter
+* sqlx migrate run
+
+You should see output like "Applied ..."
--- zero2prod/scripts/init_db.sh
+++ zero2prod/scripts/init_db.sh
@@ -1,5 +1,5 @@
-#! /usr/bin/env bash
-set -x
+#!/bin/bash
+#set -x # prints out every line when you run the script!
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
@@ -30,23 +30,33 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host name has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
+CONTAINER_NAME="zero2prod"
+
 # Launch postgres using docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_PASSWORD} \
-    -p "${DB_PORT}":5432 \
-    -d postgres -N 1000 # ^ Increase max number of connections for testing purposes
+if [ ! "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    echo "Creating Docker container (name=${CONTAINER_NAME})."
+    docker run \
+        --name ${CONTAINER_NAME} \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_PASSWORD} \
+        -p "${DB_PORT}":5432 \
+        -d postgres -N 1000 # ^ Increase max number of connections for testing purposes
+else
+    echo "Docker container already found (name=${CONTAINER_NAME})."
+fi
 
 # Keep pinging Postgres until it's readt to accept commands
 export "PGPASSWORD=${DB_PASSWORD}" # for windows you need to export "my_stuff_wrapped_in_quotes"
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q' ; do
-    >&2 echo "Postgres is still unavailable - sleeping"
+    echo "Postgres is still unavailable - sleeping"
     sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+echo "Postgres is up and running on port ${DB_PORT}!"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+echo "Postgres has been migrated, ready to go!"
